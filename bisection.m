7ddbfb4234e4656872e55b2c140cc0af14add55f
gokmavisianka <85447383+gokmavisianka@users.noreply.github.com>
2022-10-06 15:55:07 +0300

    Create bisection.m

---
+++ bisection.m
@@ -0,0 +1,33 @@
+function bisection()
+    xl = input('xl: ');
+    xu = input('xu: ');
+    TC = input('TC: ');
+    if f(xl)*f(xu)<0
+        epsilon = [];
+        xr = [];
+        i = 1;
+        while true
+            xr(i) = (xl + xu) / 2;
+            temp = f(xl) * f(xr(i));
+                
+            if temp > 0
+                xl = xr(i);
+            elseif temp < 0
+                xu = xr(i);
+            end
+                
+            if i > 2
+                epsilon(i - 1) = abs((xr(i) - xr(i-1)) / xr(i)) * 100;
+                if epsilon(i - 1) < TC
+                    fprintf("approximate root: %f, iterations: %d", xr(i), i);
+                    break;
+                end
+            end
+            i = i + 1;
+        end                            
+    end
+end
+
+function result = f(x)
+    result = exp(-x) - x
+end
