67a2380a3a242356f3b6e021d387337529ffd7eb
Rasim Mert YILDIRIM <85447383+gokmavisianka@users.noreply.github.com>
2022-10-12 13:01:45 +0300

    Update fixed_point.py

--- fixed_point.py
+++ fixed_point.py
@@ -1,29 +1,29 @@
-from math import exp
+from sympy import exp
 
 def f(x):
-	result = exp(-x)
-	return result
+    result = exp(-x)
+    return result
 
 def float_input(text):
-	while True:
-		try:
-			value = float(input(text))
-			return value
-		except Exception as exception:
-			print(exception)
+     while True:
+         try:
+             value = float(input(text))
+             return value
+         except Exception as exception:
+             print(exception)
 
 def fixed_point():
-	x = [float_input("x0: ")]
-	TC = float_input("TC: ")
-	if True:
-		epsilon = []
-		i = 1
-		while True:
-			x.append(f(x[i - 1]))
-			epsilon.append(abs((x[i] - x[i - 1]) / x[i]) * 100)
-			if epsilon[i - 1] < TC:
-				return {"approximate root": x[i], "iterations": i}
-			i += 1
-		
-print(fixed_point())
-		
+    x = [float_input("x0: ")]
+    TC = float_input("TC: ")
+    epsilon = []
+    i = 1
+    while True:
+        x.append(f(x[i - 1]))
+        epsilon.append(abs((x[i] - x[i - 1]) / x[i]) * 100)
+        if epsilon[i - 1] < TC:
+            return {"approximate root": x[i], "iterations": i}
+        i += 1
+
+if __name__ == "__main__":
+    print(fixed_point())
+    
