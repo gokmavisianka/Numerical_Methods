ae1763e11678188555e119495dfa6b280643de9b
Rasim Mert YILDIRIM <85447383+gokmavisianka@users.noreply.github.com>
2022-10-12 12:49:36 +0300

    Update newton_raphson.py

--- newton_raphson.py
+++ newton_raphson.py
@@ -5,10 +5,10 @@ def f(x):
     return result
 
 def f_prime(value):
-	x = Symbol('x')
-	y = (exp(-x) - x).diff(x)
-	result = y.subs(x, value)
-	return result
+    x = Symbol('x')
+    y = (exp(-x) - x).diff(x)
+    result = y.subs(x, value)
+    return result
 	
 	
 def float_input(text):
@@ -20,17 +20,17 @@ def float_input(text):
             print(exception)
 
 def newton_raphson():
-	x = [float_input("x0: ")]
-	TC = float_input("TC: ")
-	epsilon = []
-	i = 0
-	while True:
-		x.append(x[i] - f(x[i]) / f_prime(x[i]))
-		epsilon.append(abs((x[i + 1] - x[i]) / x[i + 1]) * 100)
-		if epsilon[i] < TC:
-			print(f"approximate root: {x[i+1]}, iterations: {i}")
-			break
-		i += 1
+    x = [float_input("x0: ")]
+    TC = float_input("TC: ")
+    epsilon = []
+    i = 0
+    while True:
+         x.append(x[i] - f(x[i]) / f_prime(x[i]))
+         epsilon.append(abs((x[i + 1] - x[i]) / x[i + 1]) * 100)
+         if epsilon[i] < TC:
+             print(f"approximate root: {x[i+1]}, iterations: {i}")
+             break
+         i += 1
 
 newton_raphson()
 
