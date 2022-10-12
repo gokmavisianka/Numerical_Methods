adcc7eef6f8c127120356654c83e1dc9e127a3c9
Rasim Mert YILDIRIM <85447383+gokmavisianka@users.noreply.github.com>
2022-10-12 12:58:58 +0300

    Update secant.py

--- secant.py
+++ secant.py
@@ -13,16 +13,17 @@ def float_input(text):
             print(exception)
 
 def secant():
-	x = [float_input("x0: "), float_input("x1: ")]
-	TC = float_input("TC: ")
-	epsilon = []
-	i = 1
-	while True:
-		x.append(x[i] - f(x[i]) * (x[i] - x[i - 1]) / (f(x[i]) - f(x[i - 1])))
-		epsilon.append(abs((x[i + 1] - x[i]) / x[i + 1]) * 100)
-		if epsilon[i - 1] < TC:
-			print(f"approximate root: {x[i+1]}, iterations: {i}")
-			break
-		i += 1
+    x = [float_input("x0: "), float_input("x1: ")]
+    TC = float_input("TC: ")
+    epsilon = []
+    i = 1
+    while True:
+        x.append(x[i] - f(x[i]) * (x[i] - x[i - 1]) / (f(x[i]) - f(x[i - 1])))
+        epsilon.append(abs((x[i + 1] - x[i]) / x[i + 1]) * 100)
+        if epsilon[i - 1] < TC:
+            return {"approximate root": x[i+1], "iterations": i}
+            break
+        i += 1
 
-secant()
+if __name__ == "__main__":
+    print(secant())
